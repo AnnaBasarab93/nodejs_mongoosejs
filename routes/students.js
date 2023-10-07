31843a3ccb619b492f404b8bf88df94ae4b86823
Anna Basarab <90757241+AnnaBasarab93@users.noreply.github.com>
2023-10-07 14:21:06 +0200

    finished project

--- routes/students.js
+++ routes/students.js
@@ -3,6 +3,7 @@ const studentsRouter = express.Router();
 import Student from '../models/Student.js';
 
 
+//GET
 
 studentsRouter.get("/", async (req, res) => {
     try {
@@ -13,11 +14,47 @@ studentsRouter.get("/", async (req, res) => {
         res.status(500).json(err)
     }
 })
+//GET :id
 
 studentsRouter.get("/:id", async (req, res) => {
     const {id} = req.params;
+
     try {
         const response = await Student.findById(id);
+    if(!response){
+        res.status(404).json({message : "Student doesn't found"})    
+    }
+    
+    res.json(response)
+
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+//POST
+studentsRouter.post("/", async (req, res) => {
+
+    try {
+        const {name, first_name, email} = req.body;
+        const response = await Student.create({name, first_name, email});
+        res.json(response)
+    } catch(err){
+        res.status(500).json(err)
+    }
+})
+
+//PUT /:id  :  To edit one user (with the id) 
+
+studentsRouter.put("/:id", async (req, res) => {
+    try {
+        const {name, first_name, email} = req.body;
+        const {id} = req.params;
+        const response = await Student.findByIdAndUpdate(id, {name, first_name, email});
+
+    if(!response){
+            res.status(404).json({message : "Student doesn't found"})    
+        }
         res.json(response)
 
     } catch(err){
@@ -25,12 +62,25 @@ studentsRouter.get("/:id", async (req, res) => {
     }
 })
 
+///PUT  : All name equal to John, change it to “Bob”
 
-studentsRouter.post("/", async (req, res) => {
-
+studentsRouter.put("/update/:name", async (req, res) => {
     try {
-        const {name, first_name, email} = req.body;
-        const response = await Student.create({name, first_name, email});
+        const {name} = req.params;
+        const response = await Student.updateMany({name}, { name: "Bob" });
+        res.json(response)
+    } catch(err){
+        res.status(500).json(err)
+    }
+})
+
+
+
+//DELETE
+studentsRouter.delete("/:id", async (req, res) => {
+    try {
+        const {id} = req.params;
+        const response = await Student.findByIdAndDelete(id);
         res.json(response)
     } catch(err){
         res.status(500).json(err)
